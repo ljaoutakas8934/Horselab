e4fe596831422a5897c4ff982eee9ec046e1a685
BT_1N3_03 <ljaoutakas8934@bths.edu>
2018-11-15 14:45:24 -0500

    commit

---
+++ src/HorseBarn.java
@@ -0,0 +1,14 @@
+public class HorseBarn {
+    private Horse[] spaces;
+    public int findHorseSpace(String name) {
+        for(int i = 0; i < this.spaces.length; i++) {
+            if (this.spaces[i] != null && name.equals(this.spaces[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void consolidate() {
+
+    }
+}
