37b98c549f2a2ba3ff6e9bbf9cb1046031522106
BT_1N3_03 <ljaoutakas8934@bths.edu>
2018-11-15 14:47:25 -0500

    commit

--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -9,6 +9,14 @@ public class HorseBarn {
         return -1;
     }
     public void consolidate() {
-
+        Horse[] newSpaces = new Horse[this.spaces.length];
+        int nextSpot = 0;
+        for (Horse nextHorse : this.spaces) {
+            if (nextHorse != null){
+                newSpaces[nextSpot] = nextHorse;
+                nextSpot++;
+            }
+        }
+        this.spaces = newSpaces;
     }
 }
